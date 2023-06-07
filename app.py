1355c633aad3063de9e90d0054f81a3b9ccc21a3
Pikachú <engbethany4@gmail.com>
2023-06-07 15:08:32 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+# use the model in a flask app
+import pickle
+
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+# load the model
+model = pickle.load(open('yelp_model.pkl', 'rb'))
+
+# load the vectorizer
+vectorizer = pickle.load(open('yelp_vectorizer.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    # predict the rating based on the review
+    if request.method == 'POST':
+        text = request.form['text']
+        my_prediction = model.predict([text])
+        prediction = f'This is a {my_prediction} star review.'
+    
+        return render_template('result.html', prediction=prediction, text=text)
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
